704df693cc54007d0556b667ddc2ce9f969dc98e
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2024-04-21 18:24:42 +0100

    starting point

--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -1,37 +1,14 @@
-import type { Config } from 'tailwindcss';
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+export default {
+  content: ["./src/**/*.tsx"],
   theme: {
-    colors: {
-      /* Color Theme Swatches in Hex */
-      primary: ' #F57C4C',
-      red: '#F54C70',
-      green: ' #4DF5A2',
-      secondary: '#404040',
-      grey: ' #5D5D5D',
-      white: '#e6e6e6',
-      gold: ' #C9B037',
-      silver: ' #B4B4B4',
-      bronze: ' #AD8A56',
-    },
-    fontFamily: {
-      custom: ['var(--font-noto_Sans_Display)'],
-      playfair: ['Playfair Display', 'serif'],
-      sans: ['Noto Sans', 'sans-serif'],
-    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-};
-export default config;
+} satisfies Config;
