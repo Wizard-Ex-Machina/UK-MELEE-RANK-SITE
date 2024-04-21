db4df34deb668767c27929c8b66be25bbc702d72
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2024-04-21 19:11:52 +0100

    build changes

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
+import { Inter, Playfair_Display, Noto_Sans_Display } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,6 +13,17 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const noto_Sans_Display = Noto_Sans_Display({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-noto_Sans_Display",
+});
+
+const playfair_display = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-playfair",
+});
 export default function RootLayout({
   children,
 }: {
