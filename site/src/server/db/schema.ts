59ea9009a17abbe22745f17834b0095ee98bd53e
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2024-04-22 15:17:50 +0100

    added schema

--- site/src/server/db/schema.ts
+++ site/src/server/db/schema.ts
@@ -1,34 +1,50 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
+  pgTable,
+  integer,
+  text,
+  date,
+  foreignKey,
+  numeric,
 } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `site_${name}`);
+export const players = pgTable("players", {
+  id: integer("id").primaryKey().notNull(),
+  name: text("name"),
+  startGgId: integer("start_gg_id"),
+  firstAppears: date("first_appears"),
+});
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const matches = pgTable("matches", {
+  id: integer("id").primaryKey().notNull(),
+  eventId: integer("event_id").references(() => events.meleeId, {
+    onDelete: "cascade",
+  }),
+  player1Id: integer("player_1_id").references(() => players.id, {
+    onDelete: "cascade",
+  }),
+  player2Id: integer("player_2_id").references(() => players.id, {
+    onDelete: "cascade",
+  }),
+  player1Score: integer("player_1_score"),
+  player2Score: integer("player_2_score"),
+  startGgId: text("start_gg_id"),
+});
+
+export const playerRatings = pgTable("player_ratings", {
+  id: integer("id").primaryKey().notNull(),
+  rating: numeric("rating"),
+  rd: numeric("rd"),
+  vol: numeric("vol"),
+  period: date("period"),
+  player: integer("player").references(() => players.id),
+});
+
+export const events = pgTable("events", {
+  id: integer("id").primaryKey().notNull(),
+  startGgId: integer("start_gg_id"),
+  name: text("name"),
+  meleeId: integer("melee_id"),
+  endAt: date("end_at"),
+  postalCode: text("postal_code"),
+});
