e0a28a978d48b75b45f82ebe3f4105e48afbfc37
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2024-11-26 21:52:04 +0000

    remove typo

--- api/internal/scraper/scraper.go
+++ api/internal/scraper/scraper.go
@@ -103,7 +103,7 @@ func Scraper() {
 															CharacterID: int32(selection.Character.Id),
 														})
 													}
-												}git
+												}
 											}
 										}
 									}
