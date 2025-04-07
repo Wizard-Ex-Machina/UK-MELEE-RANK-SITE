bab8b24b27568a5f22dcf40b4635a117159b4992
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2025-04-07 19:21:49 +0100

    Errors for missing env values

--- scraper/internal/config/config.go
+++ scraper/internal/config/config.go
@@ -13,10 +13,18 @@ func STARTGG_API_TOKEN() (string, error) {
 	return "", errors.New("STARTGG_API_TOKEN not inculded in env")
 }
 
-func PORT() string {
-	return os.Getenv("PORT")
+func PORT() (string, error) {
+	data := os.Getenv("PORT")
+	if data != "" {
+		return data, nil
+	}
+	return "", errors.New("PORT not inculded in env")
 }
 
-func DATABASE_URL() string {
-	return os.Getenv("DATABASE_URL")
+func DATABASE_URL() (string, error) {
+	data := os.Getenv("DATABASE_URL")
+	if data != "" {
+		return data, nil
+	}
+	return "", errors.New("DATABASE_URL not inculded in env")
 }
