2488ac0865e470826407d27e1584f9d3be139226
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2025-04-08 14:02:17 +0100

    new env flags

--- scraper/internal/config/config.go
+++ scraper/internal/config/config.go
@@ -18,7 +18,7 @@ func PORT() (string, error) {
 	if data != "" {
 		return data, nil
 	}
-	return "", errors.New("PORT not inculded in env")
+	return "3001", errors.New("PORT not inculded in env defaulting to 3001")
 }
 
 func DATABASE_URL() (string, error) {
@@ -27,4 +27,23 @@ func DATABASE_URL() (string, error) {
 		return data, nil
 	}
 	return "", errors.New("DATABASE_URL not inculded in env")
+}
+
+func REGION_CODE() (string, error) {
+	data := os.Getenv("REGION_CODE")
+	if data != "" {
+		return data, nil
+	}
+	return "GB", errors.New("REGION_CODE missing from env defaulting to GB")
+}
+
+func GLOBAL() (bool, error) {
+	data := os.Getenv("REGION_CODE")
+	if data == "TRUE" {
+		return true, nil
+	}
+	if data == "FALSE" {
+		return false, nil
+	}
+	return false, errors.New("GLOBAL missing from env defaulting to false")
 }
