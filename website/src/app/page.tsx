14662b39d2b1377e99ab05eac478f84b1ec57d8a
Gnothenat-The-Wizard <mrjackkefford@gmail.com>
2025-06-23 19:26:32 +0100

    start of site rewrite

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,80 +1,103 @@
-import { playfair_dislpay, bebas_neue } from "./fonts";
-import Titlebar from "./_components/Titlebar";
-import LeaderboardItem from "./_components/LeaderboardItem";
+import Image from "next/image";
 
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  async function getLeaderboard(): Promise<Player[]> {
-    const res = await fetch("https://meleeranked.uk/api/leaderboard", {
-      next: {
-        revalidate: 216000,
-      },
-    });
-    const data = await res.json();
-
-    return data;
-  }
-
-  const randomMessages = [
-    "Now with daily updates!",
-    "Now in techincolor",
-    "I forgot how much I sucked",
-    "You, again? and I thought my ego was inflated",
-    "Your rating is not gonna go up if you keep looking at this page, go win some games",
-  ];
-
-  const randomMessage =
-    randomMessages[Math.floor(Math.random() * randomMessages.length)];
-
-  const leaderboard = await getLeaderboard();
-
+export default function Home() {
   return (
-    <div>
-      <Titlebar
-        title="A Glicko2 Ranking Of People That Sometimes Play Melee In The UK"
-        subtitle={randomMessage}
-      />
-      <div className="w-full flex justify-center">
-        <div className="p-2 w-full xl:w-1/2 xl:p-0" key="tcyfvugbhinji">
-          <div
-            className={`my-4 h-12 max-w-full flex border-2 rounded-md font-bold px-4 items-center bg-gradient-to-r from-zinc-100/25 to-zinc-300/25 border-zinc-50`}
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <div className="w-16 items-center">
-              <p className="text-center">RANK</p>
-            </div>
-            <div className="w-16 items-center">
-              <p className="text-center">↑↓</p>
-            </div>
-            <div className="w-full px-1 items-center">
-              <p className="text-left">NAME</p>
-            </div>
-            <div className="w-16 items-center">
-              <p className="text-center">RD</p>
-            </div>
-            <div className="w-16 items-center">
-              <p className="text-center">↑↓</p>
-            </div>
-            <div className="w-24 items-center">
-              <p className="text-center">RATING</p>
-            </div>
-          </div>
-          {leaderboard.map((player: Player) => {
-            return <LeaderboardItem player={player} />;
-          })}
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
-
-export type Player = {
-  Name: string;
-  PlayerID: number;
-  Rank: number;
-  RankDiff: number;
-  R: number;
-  Rd: number;
-  Delta: number;
-  Percentile: number;
-};
